eeceba1506c04b74e1e31c6ad35df298175fe8c7
Daniel Hanson <danielghanson93@gmail.com>
2016-08-03 18:07:21 -0400

    added insert and erase

---
+++ src/kdtree.cpp
@@ -0,0 +1,81 @@
+//============================================================================
+// Name        : kdtree.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+#include "kdtree.h"
+
+using namespace std;
+using namespace dhlib;
+
+template<unsigned K, typename T,typename Ealloc,typename Palloc,typename Lalloc>
+template<typename coord>
+pair<const LeafNode<K,T,Ealloc>&, KDNode<K,T,Ealloc>*&> KDTree<K,T,Ealloc,Palloc,Lalloc>::getLeaf(const coord& point){
+	unsigned i (0);
+	KDNode<K,T,Ealloc> **childPtr = root, *current = root;
+	while(!current->isLeaf){
+		double eVal = point[i++];
+		ParentNode<K,T,Ealloc>* parent = (ParentNode<K,T,Ealloc>*) current;
+		if(eVal > parent->value){
+			current = parent->right;
+			childPtr = &parent->right;
+		} else {
+			current = parent->left;
+			childPtr = &parent->left;
+		}
+		if(i == K){
+			i = 0;
+		}
+	}
+	LeafNode<K,T,Ealloc>& leaf = (ParentNode<K,T,Ealloc>&) current;
+	return std::make_pair(leaf, *childPtr);
+}
+
+template<unsigned K, typename T,typename Ealloc,typename Palloc,typename Lalloc>
+template<typename coord>
+void KDTree<K,T,Ealloc,Palloc,Lalloc>::insert(const coord& point, T&& elem){
+	LeafData<K,T,Ealloc> leafData = getLeaf(point);
+	LeafNode<K,T,Ealloc>& leaf = leafData.leaf;
+	KDNode<K,T,Ealloc>*& childPtr = leafData.childPtr;
+
+	entry<K,T>& entry = *leaf.value.insert(point[leafData.i]);
+	array<double,K>& addedPoint = entry.first;
+
+	entry.second(forward(elem)); // move element into tree
+	for(unsigned i = 0; i < K; i++){ // copy coordinates into tree
+		addedPoint[i] = point[i];
+	}
+
+	if(leaf.values.size > leafSize){ // leaf is to big, we will split it now
+		LeafNode<K,T,Ealloc>* newLeft, newRight;
+		double value;
+		newLeft = Lalloc::allocate(1);
+		Lalloc::construct(newLeft);
+		auto iter = leaf.values.begin();
+		unsigned n = 0;
+		while(n++ < leaf.values.size() / 2){ // copy the left elements into the left child
+			newLeft->values.insert(*iter++);
+		}
+
+		value = iter->first; // new node's value
+
+		newRight = Lalloc::allocate(1);
+		Lalloc::construct(newRight, iter, leaf.values.end());
+		ParentNode<K,T,Ealloc>* parent = Palloc::alocate(1);
+		Palloc::construct(parent, value, *left, *right); // copy the right elements into the right child
+		*childPtr = parent; // set the parent's pointer to the new child
+	}
+}
+
+
+
+template<unsigned K, typename T,typename Ealloc,typename Palloc,typename Lalloc>
+template<typename coord>
+unsigned KDTree<K,T,Ealloc,Palloc,Lalloc>::erase(const coord& point){
+	LeafData<K,T,Ealloc> leafData = getLeaf(point);
+	LeafNode<K,T,Ealloc>& leaf = leafData.leaf;
+	KDNode<K,T,Ealloc>& childPtr = leafData.childPtr;
+	return *leaf.values.erase(point[leafData.i]);
+}
---
+++ src/kdtree.h
@@ -0,0 +1,244 @@
+/*
+ * kdtree.h
+ *
+ *  Created on: Aug 2, 2016
+ *      Author: hansondg
+ */
+
+#ifndef KDTREE_H_
+#define KDTREE_H_
+
+#include <functional>
+#include <map>
+#include <iterator>
+#include <stack>
+
+namespace dhlib {
+
+	template<unsigned K, typename T>
+	using entry = std::pair<std::array<double,K>, T>;
+
+	template<unsigned K, typename T, typename Ealloc>
+	struct KDNode;
+
+	template<unsigned K, typename T, typename Ealloc>
+	struct ParentNode;
+
+	template<unsigned K, typename T, typename Ealloc>
+	struct LeafNode;
+
+	template<
+		unsigned K, typename T,
+		typename Ealloc = std::allocator<std::pair<double, entry<K,T> > >,
+		typename Palloc = std::allocator<ParentNode<K,T,Ealloc> >,
+		typename Lalloc = std::allocator<LeafNode<K,T,Ealloc> >
+	>
+	class KDTree;
+
+	template<unsigned K, typename T>
+	using kd_iter = std::iterator<std::bidirectional_iterator_tag, entry<K,T>, std::ptrdiff_t, entry<K,T>*, entry<K,T>& >;
+
+	template<unsigned K, typename T, typename Ealloc>
+	using leaf_map = std::multimap<double, entry<K,T>, std::less<double>, Ealloc>;
+
+	template<unsigned K, typename T, typename Ealloc>
+	struct KDNode {
+		const bool isLeaf;
+		KDNode(bool isLeaf) :
+				isLeaf{isLeaf} {
+		}
+	};
+
+	template<unsigned K, typename T, typename Ealloc>
+	struct ParentNode : KDNode<K,T,Ealloc> {
+		double value;
+		const KDNode<K,T,Ealloc>* left, right;
+		ParentNode(double value, KDNode<K, T, Ealloc>& left, KDNode<K, T, Ealloc>& right) :
+				KDNode<K, T, Ealloc>(false), value{value}, left(left), right(right) { }
+	};
+
+	template<unsigned K, typename T, typename Ealloc>
+	struct LeafNode : KDNode<K,T, Ealloc> {
+		const leaf_map<K,T,Ealloc> values;
+		template<typename InputIterator>
+		LeafNode(InputIterator start, InputIterator end) : values(start, end) { }
+		LeafNode() : values() { }
+	};
+
+	template<unsigned K, typename T, typename Ealloc>
+	struct LeafData {
+		LeafNode<K,T,Ealloc>& leaf;
+		KDNode<K,T,Ealloc>*& childPtr;
+		unsigned i;
+	};
+
+	template<
+		unsigned K, typename T,
+		typename Ealloc,
+		typename Palloc,
+		typename Lalloc
+	>
+	class KDTree {
+	using leaf_iter = typename leaf_map<K,T,Ealloc>::iterator;
+	private:
+
+		// used to iterate over the elements in no particular order
+		class StdIter : kd_iter<K,T> {
+			bool isEnd;
+			const KDTree<K,T,Ealloc,Palloc,Lalloc>* const tree;
+			const std::stack<const ParentNode<K,T,Ealloc>* const, std::deque<const ParentNode<K,T,Ealloc>,Palloc>> parents;
+			const LeafNode<K,T,Ealloc>* leaf;
+			leaf_iter iter;
+
+			LeafNode<K,T,Ealloc>& seekLeft(const KDNode<K,T,Ealloc>& parent){
+				KDNode<K,T,Ealloc>* node = &parent;
+				while(!node->isLeaf){
+					auto parent = (ParentNode<K,T,Ealloc>*) node;
+					this->parents.push(parent);
+					node = parent->left;
+				}
+				return *((LeafNode<K,T,Ealloc>*) node);
+			}
+
+			LeafNode<K,T,Ealloc>& seekRight(const KDNode<K,T,Ealloc>& parent){
+				KDNode<K,T,Ealloc>* node = &parent;
+				while(!node->isLeaf){
+					auto parent = (ParentNode<K,T,Ealloc>*) node;
+					this->parents.push(parent);
+					node = parent->right;
+				}
+				return *((LeafNode<K,T,Ealloc>*) node);
+			}
+
+		public:
+
+			StdIter() : tree(nullptr), parents(), leaf(nullptr), iter(), isEnd(true) { }
+			StdIter(const KDTree& tree, bool isEnd) : tree(tree), parents(), iter() {
+				if(isEnd){
+					this->isEnd = true;
+					return;
+				}
+				auto leaf = seekLeft(*tree.root);
+				this->map = leaf->values;
+				this->iter = leaf->values.begin();
+				this->isEnd = this->iter == leaf->values.end();
+			}
+
+			StdIter(const KDTree& tree) : StdIter(tree, false) { }
+
+			StdIter& operator++(){
+				if(isEnd){ // iterator is at end and/or uninitialized
+					return *this;
+				}
+				this->iter++;
+				while(this->iter == this->map.end()){
+					KDNode<K,T,Ealloc>* child, *parent = this->leaf;
+					do {
+						if(this->stack.size() == 0){ // reached end
+							isEnd = true;
+							return *this;
+						}
+						child = parent;
+						parent = this->stack.pop();
+					} while(parent->right == child);
+					this->leaf = seekLeft(*(parent->right));
+					this->iter = this->leaf.values.begin();
+				}
+				return *this;
+			}
+
+			StdIter& operator--(){
+				if(isEnd){
+					if(this->tree == nullptr){ // iterator is uninitialized
+						return *this;
+					}
+					if(this->leaf == nullptr){ // we need to calculate the last leaf
+						this->leaf = seekRight(8(tree->root));
+						this->iter = this->leaf.end(); // we decrement iter in the rest of this function
+					}
+				}
+				while(this->iter == this->leaf->map.begin()){
+					KDNode<K,T,Ealloc>* child, parent = this->leaf;
+					do {
+						if(this->stack.size() == 0){ // decremented passed begin
+							this->leaf = seekRight(*(tree->root)); // move iterator back to front
+							this->iter = this->leaf.begin();
+							return *this;
+						}
+						child = parent;
+						parent = this->stack.pop();
+					} while(parent->left == child);
+					this->leaf = seekRight(*(parent->left));
+					this->iter = leaf->values.end();
+				}
+				this->iter--;
+				return *this;
+			}
+
+			StdIter& operator=(const StdIter& iter){
+				this(iter);
+				return *this;
+			}
+
+			StdIter& operator=(StdIter&& iter){
+				this(forward(iter));
+				return *this;
+			}
+
+			bool operator==(const StdIter& iter){
+				if(this->isEnd && iter.isEnd && this->tree == iter.tree){
+					return true;
+				}
+				return this->iter == iter.iter;
+			}
+
+			bool operator!=(const StdIter& iter){
+				return *this == iter;
+			}
+
+			entry<K,T>& operator*(){
+				return *(this->iter);
+			}
+		};
+
+		KDNode<K,T,Ealloc>* root;
+		size_t leafSize;
+
+		// gets the leaf the specified point goes to along with a reference to the pointer that points to the leaf
+		template<typename coord>
+		std::pair<const LeafNode<K,T,Ealloc>&, KDNode<K,T,Ealloc>*&> getLeaf(const coord& point);
+	public:
+		using iterator = kd_iter<K,T>;
+
+		KDTree(size_t leafSize) {
+			this->leafSize = leafSize;
+			KDNode<K,T,Ealloc>* rootPtr = Lalloc::allocate(1);
+			Lalloc::construct(rootPtr);
+			StdIter x();
+			StdIter y(x);
+			root = *rootPtr;
+		}
+
+		KDTree() : KDTree(32) { }
+
+		template<typename coord>
+		void insert(const coord&, T&& elem);
+
+		template<typename coord>
+		void insert(const coord&, const T& elem){
+			insert(move(copy(elem)));
+		}
+
+		template<typename coord>
+		unsigned erase(const coord&);
+
+		iterator begin(){
+			return new StdIter(*this);
+		}
+
+		iterator end(){
+			return new StdIter(*this, true);
+		}
+	};
+}
+#endif /* KDTREE_H_ */
